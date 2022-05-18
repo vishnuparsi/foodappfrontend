5bb498beb46a20992bacb1389ca9d28d6fa3ab38
Sree Harsha <venkatasreeharsha.gajula@cigniti.com>
2022-05-18 12:53:45 +0000

    Update global.js

--- src/path/global.js
+++ src/path/global.js
@@ -1,7 +1,4 @@
-const GlobalVariable = {
-   BASE_API_URL: process.env.REACT_APP_API_URL
-
-}
-
-
+const GlobalVariable = Object.freeze({
+BASE_API_URL:'http://3.108.178.68:8095/'
+})
 export default GlobalVariable
